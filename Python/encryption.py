362c3a62f383d72ed8d66c97e2269f05e7b12b10
Max C <maxjrcroft@hotmail.com>
2023-09-25 09:56:56 +0100

    Created a Python Encryption System
    Used Python Functions to create ways to encrypt and decrypt encoded messages.

---
+++ Python/encryption.py
@@ -0,0 +1,55 @@
+key = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u',
+
+'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c',
+
+'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k',
+
+'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S',
+
+'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A',
+
+'O':'B', 'P':'C', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I',
+
+'W':'J', 'X':'K', 'Y':'L', 'Z':'M'}
+
+
+def menu():
+    print("1. Encode a string")
+    print("2. Decode a string")
+    print("3. Quit")
+    choice = input("Enter your choice (1, 2, or 3): ")
+    if(choice == '1'):
+        encode()
+    elif(choice == '2'):
+        decode()
+    elif(choice == '3'):
+        print("Quitting the program...")
+    else:
+        print("Not a valid choice buddy!")
+        menu()
+
+
+# using the key array above, encrypt the given string
+def encode():
+    plain = input("Enter (brief) text to encrypt: ")
+    cipher = ""
+    for letter in plain:
+        if letter in key:
+            cipher += key[letter]
+        else:
+            cipher += letter
+    print(cipher)
+    menu()
+
+def decode():
+    cipher = input("Enter (brief) text to decrypt: ")
+    plain = ""
+    for letter in cipher:
+        if letter in key:
+            plain += key[letter]
+        else:
+            plain += letter
+    print(plain)
+    menu()
+
+menu()
