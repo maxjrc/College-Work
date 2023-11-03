87121c7726da778c69181261b9db02800f167bac
maxjrc <maxjrcroft@hotmail.com>
2023-11-03 15:28:24 +0000

    Update for work
    errr did way more then needed lol

--- Python/baking.py
+++ Python/baking.py
@@ -6,6 +6,8 @@ def menu():
         create()
     if option == "2":
         read()
+    if option == "3":
+        edit()
 
 def create():
     ingredients = []
@@ -46,7 +48,7 @@ def create():
     menu()
 
 def read():
-    name = input("Please enter the name of the recipe you would like to edit? ")
+    name = input("Please enter the name of the recipe you would like to read? ")
     print("Reading...")
     time.sleep(2)
     f = open(f'{name}.txt', "r")
@@ -54,6 +56,42 @@ def read():
     f.close()
     print("Read the recipe!")
 
+def edit():
+    name = input("Please enter the name of the recipe you would like to edit? ")
+    print("Loading...")
+    time.sleep(2)
+    ingredients = []
+    amount = []
+    title = input("Please enter the new name of your recipe: ")
+    people = int(input("Please enter the amount of people this will serve: "))
+    ingredientss = int(input("How many ingridents do you need? "))
+    for x in range(ingredientss):
+        ask = input("Name of Ingredient: ")
+        amountt = int(input("Please enter the amount needed: "))
+        ingredients.append(ask)
+        amount.append(amountt)
+    instructions = input("Please enter the instructions for creating this recipe: ")
+    print("Creating...")
+    lengthi = len(ingredients)
+    lengtha = len(amount)
+    output = ""
+    output2 = 0
+    for x in range(lengthi):
+        output = output + ", " + ingredients[x]
+        output2 = output2, "", str(amount[x])
+    time.sleep(2)
+    f = open(f'{name}.txt', "w")
+    f.write(f""" {people}
 
-
+    Title: {title}
+    Serves: {people}
+    Items Needed: {output}  
+    Amount Needed: {output2}         
+    Instructions: {instructions} 
+            
+            
+            
+             """ )
+    f.close()
+    print("Edited the recipe! Saved to files!")
 menu()
--- Python/studentgrades.py
+++ Python/studentgrades.py
@@ -3,33 +3,50 @@ from datetime import date
 conn = sqlite3.connect("studentdata.db")
 curr = conn.cursor()
 
-
 def menu():
-    print("Welcome to the Grading System!")
-    op = input("Please select an option: 1. Review Grades 2. Find Student 3. Add Grades ")
-    if op == "1":
-        reviewgrades()
-    if op == "2":
-        print("Not ready!")
-    if op == "3":
-        addtest()
-    else:
-        print("Not a valid option.")
-        menu()
-
+    while True:
+        print("=====================================")
+        print("Welcome to the Grading System!")
+        op = input("Please select an option: 1. Review Grades 2. Find Student 3. Add Grades 4. Delete Grades 5. Change Grades 6. Generate Report ")
+        if op == "1":
+            reviewgrades()
+        elif op == "2":
+            findstudent()
+        elif op == "3":
+            addtest()
+        elif op == "4":
+            deletetest()
+        elif op == "5":
+            changegrade()
+        elif op == "6":
+            generatereportcard()
+        else:
+            print("Not a valid option.")
 
 def reviewgrades():
-    student = input("What student do you want to review? ")
-    # get the test from the database
-    conn.execute("SELECT * FROM tests WHERE student_name = ?", (student,))
+    test = input("What test do you want to review? ")
+    curr.execute("SELECT * FROM tests WHERE test_name = ?", (test,))
     rows = curr.fetchall()
-    print(rows)
+    if len(rows) == 0:
+        print("No tests found.")
+        return
     for row in rows:
-        print(row)
-    menu()
+        print("=====================================")
+        print("Test Name: ", row[3])
+        print("Student Name: ", row[0])
+        print("Student Grade: ", scorechanger(row[2]))
+        print("Test Date: ", row[1])
+        print("Marked by: ", row[4])
+        print("=====================================")
+    conn.commit()
 
 def addtest():
     nametest = input("Please enter the name of the test? ")
+    curr.execute("SELECT * FROM tests WHERE test_name = ?", (nametest,))
+    rows = curr.fetchall()
+    if len(rows) > 0:
+        print("Test already exists.")
+        return
     tutorname = input("Please enter the name of the marker? ")
     testdate = date.today()
     students = int(input("How many students tests did you mark? "))
@@ -40,5 +57,86 @@ def addtest():
         conn.commit()
         
     print("Added the test and all grades.")
-    menu()
+
+
+def deletetest():
+    student = input("What student do you want to delete? ")
+    curr.execute("DELETE FROM tests WHERE student_name = ?", (student,))
+    conn.commit()
+    print("Deleted the test and all grades.")
+
+def changegrade():
+    student = input("What student do you want to change? ")
+    conn.execute("SELECT * FROM tests WHERE student_name = ?", (student,))
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+    test = input("What test do you want to change? ")
+    newgrade = input("What is the new grade? ")
+    conn.execute("UPDATE tests SET score = ? WHERE student_name = ? AND test_name = ?", (newgrade, student, test))
+    conn.commit()
+    print("Changed the grade.")
+
+def findstudent():
+    student = input("What student do you want to find? ")
+    curr.execute("SELECT * FROM tests WHERE student_name = ?", (student,))
+    rows = curr.fetchall()
+    for row in rows:
+        print("=====================================")
+        print("Test Name: ", row[3])
+        print("Score: ", row[2])
+        print("Grade: ", scorechanger(row[2]))
+        print("Test Date: ", row[1])
+        print("Marked by: ", row[4])
+        print("=====================================")
+
+
+def scorechanger(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+    
+def generatereportcard():
+    student = input("What student do you want to generate a report card for? ")
+    curr.execute("SELECT * FROM tests WHERE student_name = ?", (student,))
+    rows = curr.fetchall()
+    # create a text file with the student name
+    file = open(student + ".txt", "w")
+    file.write("Report Card\n")
+    file.write("Student Name: " + student + "\n")
+    total = 0
+    for row in rows:
+        total += row[2]
+        average = total / len(rows)
+        average = scorechanger(average)
+        message = checkifgraduate(average)
+    file.write("Year Grade: " + average + "\n")
+    file.write("=====================================\n")
+    file.write("Test Results\n")
+    for row in rows:
+        file.write("Test Name: " + row[3] + "\n")
+        file.write("Grade: " + scorechanger(row[2]) + "\n")
+        file.write("=====================================\n")
+    file.write(message)
+    file.close()
+    print("Generated the report card.")
+
+def checkifgraduate(average):
+    if average == "A":
+        return "You have graduated congratulations and we wish you the best of luck in your future endeavors."
+    elif average == "B":
+        return "You have graduated congratulations and we wish you the best of luck in your future endeavors."
+    elif average == "C":
+        return "You have graduated congratulations and we wish you the best of luck in your future endeavors."
+    elif average == "D":
+        return "You have graduated congratulations and we wish you the best of luck in your future endeavors."
+    else:
+        return "Unfortunately you have not graduated, we have sent you a letter with more information about the next steps you can take to graduate. You may appeal your marks by contacting your tutor."
 menu()
