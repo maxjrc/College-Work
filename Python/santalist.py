40cff7c2d0b88c428b5e40fd0efa93c6cf2ec5c9
Max C <58003230+maxjrc@users.noreply.github.com>
2023-11-20 13:11:05 +0000

    ssds

--- Python/santalist.py
+++ Python/santalist.py
@@ -1,6 +1,12 @@
-import sqlite3
-import time
 
+def start():
+    print("Welcome to Santa's list!")
+    name = input("Please enter your name to see if you are on the nice list: ")
+    print("How nice are you on a scale of 1-10?")
+    nice = int(input("Nice: "))
+    if nice >= 5:
+        print(name, "you're on the nice list!")
+    else:
+        print(name, "you're on the naughty list!")
 
-conn = sqlite3.connect("santaslist.db")
-curr = conn.cursor()
+start()
